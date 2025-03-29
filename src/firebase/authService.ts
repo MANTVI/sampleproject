0f32043229acb7a088951223b810e2f5e3218b6a
MANTVI <suresh.sc123.123@gmail.com>
2025-03-29 11:05:34 +0530

    update

--- src/firebase/authService.ts
+++ src/firebase/authService.ts
@@ -1,7 +1,7 @@
 
-import  firebaseConfig from "@/configFirebase"
+import firebaseConfig from "@/configFirebase"
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut,updateProfile } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 
 
 
@@ -11,38 +11,51 @@ const app = initializeApp(firebaseConfig);
 
 
 export class AuthService {
- 
+
   auth;
-  
+
 
   constructor() {
     this.auth = getAuth(app);
 
-   
+
   }
 
- 
+
   async createAccount({ email, password, name }: { email: string; password: string; name?: string }) {
     try {
+      let signInCheck = false;
+      try {
+        await signInWithEmailAndPassword(this.auth, email, password);
+        signInCheck = true;
+      } catch {
+        signInCheck = false;
+      }
+
+      if (signInCheck) {
+        throw new Error("An account with this email already exists. Please log in instead.");
+      }
       const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
       const user = userCredential.user;
 
-     if(user){
-      await updateProfile(user, { displayName: name });
-      await this.auth.currentUser?.reload();
-     }
-      
+      if (user) {
+        await updateProfile(user, { displayName: name });
+        await this.auth.currentUser?.reload();
+      }
 
-      if(userCredential) return await this.login({ email, password });
+
+      if (userCredential) return await this.login({ email, password });
     } catch (error) {
-      console.error("Error in createAccount:", error);
+      // console.error("Error in createAccount:", error);
       throw error;
     }
   }
 
- 
+
   async login({ email, password }: { email: string; password: string }) {
     try {
+
+
       const userCredential = await signInWithEmailAndPassword(this.auth, email, password);
       return userCredential.user;
     } catch (error) {
@@ -51,33 +64,33 @@ export class AuthService {
     }
   }
 
-  
-  
+
+
   async getCurrentUser() {
     try {
-        
-        const user = this.auth.currentUser;
-       
-        return user;
+
+      const user = this.auth.currentUser;
+
+      return user;
     } catch (error) {
-        console.error("Error in getCurrentUser:", error);
-        
+      console.error("Error in getCurrentUser:", error);
+
     }
     return null;
-}
+  }
 
 
- 
+
   async logout() {
     try {
-      await signOut(this.auth);  
+      await signOut(this.auth);
       sessionStorage.clear();
       localStorage.clear();
     } catch (error) {
       console.error("Error in logout:", error);
     }
   }
-  
+
 }
 
 
