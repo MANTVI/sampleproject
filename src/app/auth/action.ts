4f39465550f690c778cd764b803518e494a6e2e3
MANTVI <suresh.sc123.123@gmail.com>
2025-04-10 10:29:56 +0530

    update action handler for auth login and logout

--- src/app/auth/action.ts
+++ src/app/auth/action.ts
@@ -1,8 +1,10 @@
 
 'use server';
 import { setSessionCookie ,removeSessionCookie} from "@/lib/auth/cookies";
+import type { User } from 'firebase/auth';
 
-export async function handleFirebaseLogin(user: any) {
+// import type { User } from 'firebase/auth';
+export async function handleFirebaseLogin(user: User) {
   const token = await user.getIdToken(); 
   setSessionCookie(token);              
 }
